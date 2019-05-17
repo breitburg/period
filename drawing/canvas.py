2a9d150d6e9bfd4c629e382dd71196188ffcb4ea
ketsu8 <ilya.breytburg@gmail.com>
2019-05-17 21:27:48 +0300

    Moved drawing module to special files

---
+++ drawing/canvas.py
@@ -0,0 +1,11 @@
+from luma.core.render import canvas
+from drawing.advanced import AdvancedDraw
+
+
+class Canvas(canvas):
+    def __init__(self, device):
+        super(Canvas, self).__init__(device=device)
+
+    def __enter__(self):
+        self.draw = AdvancedDraw(self.image, self.device)
+        return self.draw
