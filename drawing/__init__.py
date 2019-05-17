c64f66f9dfbc8dad04fcfdae616d9eeadc7037ee
ketsu8 <ilya.breytburg@gmail.com>
2019-05-17 20:09:16 +0300

    Added Progress Bar Optionality and created built-in fonts variables

--- drawing/__init__.py
+++ drawing/__init__.py
@@ -1,4 +1,4 @@
-from PIL import ImageDraw, ImageFont
+from PIL import ImageDraw
 from luma.core.render import canvas
 
 
@@ -16,18 +16,37 @@ class AdvancedDraw(ImageDraw.ImageDraw):
         super(AdvancedDraw, self).__init__(image, mode='1')
         self.device = device
 
-    def alert(self, text='Произошла ошибка', font=ImageFont.truetype('fonts/text.ttf', 9), icon='error', duration=4):
+        from PIL import ImageFont
+        self.text_font = ImageFont.truetype('fonts/text.ttf', 9)
+        self.caption_font = ImageFont.truetype('fonts/caption.ttf', 9)
+
+    def alert(self, text='An error has occurred', font=None, icon='error', duration=5, progress_bar=True,
+              move_stap=1.5):
+        font = font if font else self.text_font
         from PIL import Image
         from time import time
 
-        start_time = time()
-        pixels_step = round(127 / duration)
+        start_time = None
+        pixels_step = round(self.device.size[0] / duration)
+        elements_positions = 0 - self.textsize(text=text, font=font)[0]
+        assert move_stap <= 10
 
         while True:
             with canvas(self.device) as draw:
-                remaining_time = start_time + duration - time()
-                if remaining_time <= 0: break
+                left_time = None
+                in_animation = elements_positions < 10
+                if in_animation: elements_positions += move_stap
 
-                draw.bitmap((10, 15), Image.open('icons/{icon}.png'.format(icon=icon)), fill=255)
-                draw.text((10, 35), text=str(text), font=font, fill=255)
-                draw.line([0, 63, (127 - round(remaining_time * pixels_step)), 63], fill=255)
+                if not in_animation:
+                    if not start_time: start_time = time()
+                    left_time = start_time + duration - time()
+                    if left_time <= 0: break
+
+                if progress_bar and left_time: draw.line([
+                    0,
+                    self.device.size[1] - 1,
+                    self.device.size[0] - round(left_time * pixels_step),
+                    self.device.size[1] - 1], fill=255)
+
+                draw.bitmap((elements_positions, 15), Image.open('icons/{icon}.png'.format(icon=icon)), fill=255)
+                draw.text((elements_positions, 35), text=str(text), font=font, fill=255)
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from core import *
 from time import sleep
+from random import randint
 
-while True:
-    with Canvas(Device()) as draw:
-        draw.alert()
-        draw.line([0, 0, 127, 63], fill=255)
+device = Device()
+
+try:
+    intager = 1
+    while True:
+        with Canvas(device) as draw:
+            intager += 1
+            if intager >= 500: intager = '1'
+            draw.rectangle([
+                randint(0, 64),
+                randint(0, 128),
+                randint(0, 64),
+                randint(0, 128)
+            ], fill=255)
+except Exception as ex:
+    with Canvas(device) as draw:
+        draw.alert(text=str(ex), duration=5)
