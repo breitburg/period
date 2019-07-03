9e89b3d4d6ad5d28556a8d27df802154a69eba4c
breitburg <contact@breitburg.me>
2019-07-03 04:16:11 +0300

    Little images refactoring

--- core/icons.py
+++ core/icons.py
@@ -1,17 +1,32 @@
 from PIL import Image
 from os import listdir
+from os.path import join
 
-class IconPack:
+
+class Pictogram:
+    def __init__(self, image: Image):
+        assert image.size == (8, 8)
+        self.image = image
+
+
+class Icon:
+    def __init__(self, image: Image):
+        assert image.size == (16, 16)
+        self.image = image
+
+
+class ImagePack:
     def __init__(self, path):
-        self.icons = {}
+        self.images = {}
 
-        for icon_path in listdir(path):
-            if not icon_path.endswith('.png'): continue
-            self.icons[icon_path.replace('.png', '')] = Image.open(f'{path}/{icon_path}')
+        for image_path in listdir(path):
+            if image_path.endswith('.png'):
+                image = Image.open(join(path, image_path))
+                self.images[image_path.replace('.png', '')] = Pictogram(image) if image.size == (8, 8) else Icon(image)
 
     def get(self, name):
-        return self.icons[name]
+        return self.images[name].image
 
 
-icons = IconPack('icons/')
-pictograms = IconPack('icons/pictograms')
+icons = ImagePack('images/')
+pictograms = ImagePack('images/pictograms')
--- main.py
+++ main.py
@@ -3,11 +3,4 @@ device = Device()
 
 while True:
     with Canvas(device) as draw:
-        from random import randint
-        from time import sleep
-
-        for progress in range(0, 101):
-            draw.progress_bar(text=f'Обновление... {progress}%', value=progress)
-            sleep(randint(0, 10) / 50)
-
         draw.alert(text='Обновление завершено!')
