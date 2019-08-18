aae789c828f21b5f0bfb2856466519906a465b63
breitburg <contact@breitburg.me>
2019-08-18 23:09:19 +0300

    Added setup file for PyPi

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('readme.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='minute',
+    version='0.0.1',
+    author='Breitburg Elias',
+    author_email='contact@breitburg.me',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/breitburg/minute',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
