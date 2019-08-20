9adaaa8a97f1b0e14f48d377eb4b2b3bb94b7858
breitburg <contact@breitburg.me>
2019-08-20 19:25:12 +0300

    Renaming buttons

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ def on_load():
 @period.on_tick
 def on_tick():
     speed = 1
-    if period.buttons.first in period.buttons.get_pressed(): speed = 2
-    elif period.buttons.second in period.buttons.get_pressed(): speed = 3
-    elif period.buttons.third in period.buttons.get_pressed(): speed = 4
-    elif period.buttons.center in period.buttons.get_pressed(): period.draw.rectangle(xy=(0, 0, 10, 10), fill=True)
-    if period.buttons.up in period.buttons.get_pressed(): period.public.position[1] -= speed
-    elif period.buttons.down in period.buttons.get_pressed(): period.public.position[1] += speed
-    elif period.buttons.left in period.buttons.get_pressed(): period.public.position[0] -= speed
-    elif period.buttons.right in period.buttons.get_pressed(): period.public.position[0] += speed
+    if period.button.first in period.button.get_pressed(): speed = 2
+    elif period.button.second in period.button.get_pressed(): speed = 3
+    elif period.button.third in period.button.get_pressed(): speed = 4
+    elif period.button.center in period.button.get_pressed(): period.draw.rectangle(xy=(0, 0, 10, 10), fill=True)
+    if period.button.up in period.button.get_pressed(): period.public.position[1] -= speed
+    elif period.button.down in period.button.get_pressed(): period.public.position[1] += speed
+    elif period.button.left in period.button.get_pressed(): period.public.position[0] -= speed
+    elif period.button.right in period.button.get_pressed(): period.public.position[0] += speed
     period.draw.point(xy=period.public.position, fill=True)
 
 if __name__ == '__main__':
