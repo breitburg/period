5d7f9c41536de665a7f21dda3ab172936de7d28e
breitburg <contact@breitburg.me>
2019-08-23 17:20:36 +0300

    Added run function to command line utility

--- period/cli/__init__.py
+++ period/cli/__init__.py
@@ -1,13 +1,15 @@
-from sys import argv
+from sys import argv, path as sys_path
 from os import getcwd, mkdir
 from os.path import join
-from json import dumps
+from json import dumps, loads
 from hashlib import md5
 from time import time
 
 argv = argv[1:]
+
 if argv[0] == '--help':
     print('There is no help!')
+
 elif argv[0] == 'new-project':
     path = join(getcwd(), argv[1])
     print(f'Creating new project {argv[1]} in {path}')
@@ -29,4 +31,14 @@ elif argv[0] == 'new-project':
     mkdir(path=join(path, 'resources'))
 
     open(file=join(path, 'src', '__init__.py'), mode='w').write(
-        'import period\n\n@period.on_start\ndef on_load():\n\tpass\n\n@period.on_tick\ndef on_frame():\n\tperiod.draw.text(xy=(0, 0), text=\'Hello, world\', fill=True)\n\nperiod.run_app()')
+        'import period\n\n@period.on_start\ndef on_load():\n\tpass\n\n@period.on_tick\ndef on_frame():\n\tperiod.draw.text(xy=(0, 0), text=\'Hello, world\', fill=True)')
+
+elif argv[0] == 'run':
+    path = getcwd()
+    manifest = loads(open(file=join(path, 'package.json'), mode='r').read())
+    print(f'Running {manifest.get("name")}...')
+
+    sys_path.insert(0, path)
+
+    app = __import__('src')
+    app.period.run_app()
