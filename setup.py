3f4297522edd65a2e1b5762e6985636e9f91704d
breitburg <contact@breitburg.me>
2019-08-23 15:20:29 +0300

    Fixed assets

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import setuptools
+from pathlib import Path
 
 setuptools.setup(
     name='period',
     version='0.0.2',
     author='Breitburg Elias',
     author_email='contact@breitburg.me',
-    long_description=open('readme.md', 'r').read(),
+    long_description=open(str(Path(__file__).resolve().parent.joinpath('readme.md')), 'r').read(),
     long_description_content_type='text/markdown',
     include_package_data=True,
     url='https://github.com/breitburg/period',
@@ -15,5 +16,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=open('requirements.txt', 'r').read().split('\n')
+    install_requires=open(str(Path(__file__).resolve().parent.joinpath('requirements.txt')), 'r').read().split('\n')
 )
