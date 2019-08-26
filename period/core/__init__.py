9c0a73fe5bc2330477e23bb90719de655dd6bb82
breitburg <contact@breitburg.me>
2019-08-26 21:23:19 +0300

    Cool display dim refactoring

--- period/core/__init__.py
+++ period/core/__init__.py
@@ -4,6 +4,7 @@ def run_app():
     from period.graphics.statusbar import status_bar
     from period.core.config import __configuration
     from period.core.draw import draw
+    from period.core.device import is_emulator
     from period.graphics.debug import fps
     from period.graphics.error import error
     from period.core.button import get_pressed
@@ -23,9 +24,9 @@ def run_app():
             # Drawing status bar
             if __configuration.get('show_status_bar'): status_bar()
             if __configuration.get('show_fps'): fps(round(1 / (time() - start_time), 1))
-            if get_pressed() == []:
+            if not get_pressed() :
                 if last_buttons_pressed_time + 2 <= start_time:
-                    draw.device.contrast(10)
+                    draw.device.contrast(80 if is_emulator else 1)
             else:
                 last_buttons_pressed_time = start_time
                 draw.device.contrast(255)
