1ecdaeb072aebdc3dd2ca918621ba21a624f4302
breitburg <contact@breitburg.me>
2019-08-28 19:24:22 +0300

    Refactored status bar

--- period/core/config.py
+++ period/core/config.py
@@ -1,10 +1,12 @@
 __configuration = {
     'show_status_bar': True,
     'auto_dim': True,
-    'show_fps': False
+    'show_fps': False,
+    'bar_height': 9  # Default is 9 pixels
 }
 
 
 def set_status_bar(mode): __configuration['show_status_bar'] = mode
 def set_fps(mode): __configuration['show_fps'] = mode
 def set_auto_dim(mode): __configuration['auto_dim'] = mode
+def set_bar_height(mode): __configuration['bar_height'] = mode
--- period/graphics/statusbar.py
+++ period/graphics/statusbar.py
@@ -1,18 +1,19 @@
 from period.core.draw import draw
 from period.core.font import icons
+from period.core.config import __configuration
 from datetime import datetime
 
 
-def status_bar(bar_height=9):
+def status_bar(bar_height=__configuration['bar_height']):
     for pixel in range(draw.device.size[0]):
         if pixel % 3 == 0: draw.point(xy=(pixel, bar_height), fill=True)
 
     time = datetime.now()
     text = f'{time.hour}:{time.minute}'
-    padding = (draw.device.size[0] - draw.textsize(text=text)[0]) / 2
+    horizontal_padding = (draw.device.size[0] - draw.textsize(text=text)[0]) / 2
 
-    draw.text(xy=(padding, -2), text=text, fill=True)
+    draw.text(xy=((draw.device.size[0] - draw.textsize(text=text)[0]) / 2, (bar_height - draw.textsize(text=text)[1]) / 2 - 1), text=text, fill=True)
 
     # When real physical device will have a battery with the ability to
     # get current voltage, we will display small battery icon at the right side in status bar
-    draw.icon(xy=(117, 0), icon=icons.get('battery-full'), size=8, fill=True)
+    draw.icon(xy=(draw.device.size[0] - 11, (bar_height - 6) / 2 - 1), icon=icons.get('battery-full'), size=8, fill=True)
