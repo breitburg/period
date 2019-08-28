0106a8e01e16a9ce8f4ca49c03b6f36ca6134310
breitburg <contact@breitburg.me>
2019-08-29 02:22:17 +0300

    Implemented menu

--- period/graphics/menu.py
+++ period/graphics/menu.py
@@ -2,15 +2,39 @@ from period.core.draw import draw
 from period.core.util import delay
 from period.graphics.statusbar import status_bar
 from period.core.config import __configuration
+from period.core.button import get_pressed, up, down, center
+
+offset = __configuration['bar_height'] + 1
+height = (draw.device.size[1] - offset) / 3
 
 
-def menu(items):
+def menu(items) :
     selected = 0
 
-    # TODO: Implement menu
+    while True :
+        display_items = [selected - 1, selected, selected + 1]
+        draw.rectangle(xy=(
+            0,
+            offset + height,
+            draw.device.size[0],
+            offset + height * 2
+        ), fill=True)
 
-    while True:
+        for item in display_items :
+            if item < 0 or item > len(items) - 1 : continue
+            draw.text(xy=(
+                0,
+                offset + (height * (display_items.index(item) + 1)) - height
+            ), text=items[item], fill=item != selected)
+
+        buttons_pressed = get_pressed()
+        if up in buttons_pressed :
+            if selected > 0 : selected -= 1
+        if down in buttons_pressed :
+            if selected < len(items) - 1 : selected += 1
+        if center in buttons_pressed :
+            return selected
+
         status_bar()
-        draw.rectangle(xy=(0, __configuration['bar_height'] + 1, draw.device.size[0], (draw.device.size[1] + 1 - __configuration['bar_height'])), fill=True)
         draw.device.apply_actions()
         draw.clear()
