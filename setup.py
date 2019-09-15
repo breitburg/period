c73937c4d8dd66c71c88ab5447cea31ed675c1ae
breitburg <contact@breitburg.me>
2019-09-15 14:49:30 +0300

    Moved version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-from period.core.version import __version__
+from period.version import __version__
 
 setup(
     name='period',
